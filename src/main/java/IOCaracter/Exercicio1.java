10e142ddca520f8faf5c25aa5cf7aa8cb452ad50
caiobarbieri <caiolvbarbieri@pm.me>
2021-12-27 11:50:14 -0300

    Exercicio1

--- src/main/java/IOCaracter/Exercicio1.java
+++ src/main/java/IOCaracter/Exercicio1.java
@@ -1,15 +1,26 @@
 package IOCaracter;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
+// Abra o teclado para escrever 3 filmes que você recomendaria e exiba esses filmes no console
 public class Exercicio1 {
     public static void InOutConsole() throws IOException {
 
+        System.out.println("Recomende 3 filmes: ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do {
+            bw.write(line);
+            bw.newLine();
+            line = br.readLine();
+        } while (!(line.isEmpty()));
+        bw.flush();
+
+        br.close();
+        bw.close();
 
     }
 
